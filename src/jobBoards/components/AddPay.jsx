189be4de1eac3a03998fa45e3b3f77b4ec52ba6f
yasir <azmyasir250@gmail.com>
2024-06-27 16:38:20 +0500

    rearrange Input and solve some error

--- src/jobBoards/components/AddPay.jsx
+++ src/jobBoards/components/AddPay.jsx
@@ -1,78 +1,93 @@
-import React from 'react';
+import React, { memo } from 'react';
 import SelectField from './SelectField';
 import InputField from './InputField';
+import CheckboxGroup from "../components/CheckboxGroup"
+import { payOptions, rateOptions, CompensationOptions , benefitsOptions } from "../utils/helper"
+import CIcon from '@coreui/icons-react';
+import { cilGrain } from "@coreui/icons"
 
-function AddPay(props) {
-    const { watch, errors, register } = props;
+
+const AddPay = ({ watch, errors, register, control }) => {
     const showPayBy = watch("ShowPayBy");
+
+    const renderInputField = (name, label) => (
+
+        <InputField
+            type="number"
+            register={register}
+            name={name}
+            label={label}
+            errors={errors}
+            watch={watch}
+        />
+    );
+
+    const renderSelectField = (name, label, options) => {
+        return (
+
+            <SelectField
+                register={register}
+                name={name}
+                label={label}
+                options={options}
+                errors={errors}
+                watch={watch}
+            />
+        )
+    }
+
+    const renderCheckboxGroup = (name, label, options) => (
+        <CheckboxGroup
+            control={control}
+            name={name}
+            label={label}
+            options={options}
+            errors={errors}
+            watch={watch}
+        />
+    );
 
     return (
 
-        <div className=''>
-            <SelectField
-                register={register}
-                name="ShowPayBy"
-                label="Show pay by"
-                options={[
-                    { value: "Range", label: "Range" },
-                    { value: "Starting amount", label: "Starting amount" },
-                    { value: "Maximum amount", label: "Maximum amount" },
-                    { value: "Exact amount", label: "Exact amount" },
-                ]}
-                errors={errors}
-                watch={watch}
-            />
-
-            {/* Conditional rendering based on "ShowPayBy" value */}
-            {showPayBy === "Range" && (
-                <div>
+        <div className='addPay_benefits'>
+            <h2 className='my-3 shadow-sm rounded py-2 px-3'>Add pay and benefits</h2>
+            <div className='addPay'>
+                {renderSelectField("ShowPayBy", "Show pay by", payOptions)}
+                {/* Conditional rendering based on "ShowPayBy" value */}
+                {showPayBy === "Range" && (
                     <div className=''>
-                        <InputField
-                            type="number"
-                            register={register}
-                            name="Maximum"
-                            label="Maximum"
-                            errors={errors}
-                            watch={watch}
-                        />
+                        {renderInputField("maximum", "Maximum")}
                         <span>to</span>
-                        <InputField
-                            type="number"
-                            register={register}
-                            name="Minimum"
-                            label="Minimum"
-                            errors={errors}
-                            watch={watch}
-                        />
+                        {renderInputField("minimum", "Minimum")}
                     </div>
+                )}
+
+                {showPayBy && showPayBy !== "Range" && renderInputField("exact", "Amount")}
+                {renderSelectField("rate", "Rate", rateOptions)}
+
+            </div>
+            {renderCheckboxGroup("CompensationPackage", "Compensation package", CompensationOptions)}
+            <div className="benefits">
+                <h2 className='my-3 shadow-sm rounded py-2 px-3'>Benefits</h2>
+                <div className=" d-flex gap-1">
+                    <CIcon icon={cilGrain} className='mx-2 ' style={{ cursor: "pointer", color: "goldenrod" }} />
+                    <div>
+                        <p> <b>Tip:</b>  Use Competitive benefits </p>
+                        <p>We've selected competitive benefits for Web Developer jobs in Seattle, WA. The higher the percentage, the more jobs have these benefits. Simply remove those you don't offer.</p>
+                        {renderCheckboxGroup("benefits", "Are any of the following benefits also offered?", benefitsOptions)}
+
+
+
+                    </div>
+
                 </div>
-            )}
 
-            {showPayBy && showPayBy !== "Range" && (
-                <InputField
-                    type="number"
-                    register={register}
-                    name="Exact"
-                    label="Exact"
-                    errors={errors}
-                    watch={watch}
-                />
-            )}
 
-            <SelectField
-                register={register}
-                name="Rate"
-                label="Rate"
-                options={[
-                    { value: "per hour", label: "per hour" },
-                    { value: "per day", label: "per day" },
-                    { value: "per week", label: "per week" },
-                ]}
-                errors={errors}
-                watch={watch}
-            />
+
+
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default AddPay;
