bcec78758a6093814e88eb1433c890f8da394683
yasir <azmyasir250@gmail.com>
2024-06-27 20:13:00 +0500

    complet Preferences-component && and add active-job component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Form from './jobBoards/components/Form'
-import '@coreui/coreui/dist/css/coreui.min.css'
+import '@coreui/coreui/dist/css/coreui.min.css';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import ActiveJob from './jobBoards/active/ActiveJob';
 
 function App() {
   return (
-    <div>
-      <Form/>
-    </div>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element = {<Form />} />
+  <Route path='/active-jobs' element = {<ActiveJob />} />
+</Routes>
+</BrowserRouter>
+    // <div>
+    //   <Form/>
+    // </div>
   )
 }
 
